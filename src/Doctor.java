43205afb621d487ff8abdac419fba6d57b278140
BurakYildrm <yldrmburak1167@gmail.com>
2021-06-22 16:41:18 +0300

    Menu removed from Receptionist.java to HospitalManagementSystem.java, getValidInput method removed and some getters added

--- src/Doctor.java
+++ src/Doctor.java
@@ -111,81 +111,11 @@ public class Doctor extends AbstractPerson implements Staff, Comparable<Doctor>{
         return "Doctor\n" + super.toString() + "\nDepartment: " + department + "\n";
     }
 
-    public void menu(){
-        Patient current = null;
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        int timeDifference = LocalDateTime.now().getDayOfYear() - appointments.getFirst().getTime().getDayOfYear();
-
-        for(int j = 0; j < timeDifference; j++){
-            for (int i = 0; i < 14; i++){
-                appointments.remove();
-            }
-            LocalDateTime date = appointments.getLast().getTime();
-            if(date.getDayOfWeek().equals(DayOfWeek.FRIDAY)){
-                date = date.plusDays(2);
-            }
-            date = date.plusDays(1).toLocalDate().atTime(9, 0);
-            for (int i = 0; i < 14; i++){
-                appointments.add(new PolyclinicAppointment(null, this, date, this.department));
-                date = date.plusMinutes(30);
-                if(date.getHour()*60 + date.getMinute() == 720){
-                    date = date.plusMinutes(60);
-                }
-            }
-        }
-
-        while (choice != 7) {
-            System.out.println("Welcome " + name + "\n1)Call the next patient\n2)View the medical information of the current patient\n" +
-                    "3)View the previous appointments of the current patient\n4)View today's appointments\n" +
-                    "5)View inpatients\n6)Clear today's schedule\n7)Exit");
-            choice = getValidInput(scanner, 1, 7);
-            switch (choice) {
-                case 1 -> {
-                    if(current != null) {
-                        PolyclinicAppointment appointment = (PolyclinicAppointment) current.getAppointments().peek();
-                        appointment.setStatus(StatusType.FINISHED);
-                    }
-                    current = callPatient();
-                }
-                case 2 -> {
-                    if (current == null) {
-                        System.out.println("You have to call a patient first.");
-                    } else{
-                        viewPatientMedicalInfo(current);
-                    }
-                }
-                case 3 -> {
-                    if (current == null) {
-                        System.out.println("You have to call a patient first.");
-                    } else{
-                        viewPatientPrevAppointments(current);
-                    }
-                }
-                case 4 -> viewAppointments();
-                case 5 -> viewInpatients(new HospitalManagementSystem());// daha sonra this olucak
-                case 6 -> clearSchedule();
-                case 7 -> System.out.println("GOODBYE");
-            }
-        }
+    public LinkedList<PolyclinicAppointment> getAppointments() {
+        return appointments;
     }
 
-    private int getValidInput(Scanner scanner, int bottomBound, int topBound){
-        int input = -1;
-        int check;
-        do {
-            check = 0;
-            try {
-                input = Integer.parseInt(scanner.nextLine());
-                if(input < bottomBound || input > topBound){
-                    System.out.println("Please enter an integer between " + bottomBound + " and " + topBound);
-                }
-            }
-            catch (NumberFormatException e){
-                System.out.println("Please enter an integer between " + bottomBound + " and " + topBound);
-                check = 1;
-            }
-        } while (input < bottomBound || input > topBound || check == 1);
-        return input;
+    public Department getDepartment() {
+        return department;
     }
 }
